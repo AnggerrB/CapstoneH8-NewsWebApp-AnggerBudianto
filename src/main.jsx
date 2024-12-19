7c2ab56254b0bf68422c8a4f4fe982b6f81c0810
angger budianto <Angger Budianto>
2024-12-19 15:10:54 +0700

    Fourth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import LayoutContainer from "./container/LayoutContainer";
+import IndonesiaPage from "./pages/IndonesiaPage";
+import ErrorPage from "./error-page";
+import ProgrammingPage from "./pages/ProgrammingPage";
+import CovidPage from "./pages/CovidPage";
+import DetailNewsPage from "./pages/DetailNewsPage";
+import BookmarksPage from "./pages/BookmarksPage";
+import SearchPage from "./pages/SearchPage";
+import HomePage from "./pages/HomePage";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutContainer />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/homePage",
+        element: <HomePage />,
+      },
+      {
+        path: "/indonesiaPage",
+        element: <IndonesiaPage />,
+      },
+      {
+        path: "/programmingPage",
+        element: <ProgrammingPage />,
+      },
+      {
+        path: "/covidPage",
+        element: <CovidPage />,
+      },
+      {
+        path: "/news/:keyword",
+        element: <SearchPage />,
+      },
+      {
+        path: "/bookmarksPage",
+        element: <BookmarksPage />,
+      },
+      {
+        path: "/detailNews/:id",
+        element: <DetailNewsPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
